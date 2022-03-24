f01670cdbc50e936f41f37f3af851cb1e1457275
John DeWyze <john@dewyze.dev>
2022-03-24 15:24:31 -0500

    Update install.sh again

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 rake
 
-if [[ -v SPIN ]]; then
-  if [ -f /etc/spin/secrets/copilot_hosts.json ]; then
-    mkdir -p "${HOME}/.config/github-copilot"
-    ln -s github_copilot_terms.json "${HOME}/.config/github-copilot/terms.json"
-    cp /etc/spin/secrets/copilot_hosts.json "${HOME}/.config/github-copilot/hosts.json"
-  fi
+if [ -f /etc/spin/secrets/copilot_hosts.json ]; then
+  mkdir -p "${HOME}/.config/github-copilot"
+  ln -s github_copilot_terms.json "${HOME}/.config/github-copilot/github_copilot_terms.json"
+  cp /etc/spin/secrets/copilot_hosts.json "${HOME}/.config/github-copilot/hosts.json"
 fi
